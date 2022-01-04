7a6feb06071f27b66b7be2d1cdd88f49aaab2ba9
Deemjan <orda1500@gmail.com>
2022-01-04 13:35:25 +0500

    На время убрал авторизацию и добавил cors middleware

--- app/main.py
+++ app/main.py
@@ -4,8 +4,18 @@ from fastapi import FastAPI
 from db.dispatcher import init_db
 from src.views.views import router
 from src.views.auth_views import auth_router
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
 app.include_router(auth_router)
 
@@ -13,14 +23,7 @@ app.include_router(auth_router)
 @app.on_event("startup")
 async def on_startup():
     await init_db()
-    # if not database.is_connected:
-    #     await database.connect()
-#
-#
-# @app.on_event("shutdown")
-# async def on_startup():
-#     if database.is_connected:
-#         await database.disconnect()
+
 
 if __name__ == "__main__":
     uvicorn.run(app)
